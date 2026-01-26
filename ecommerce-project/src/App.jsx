74f1c64ad012a1c4471a3e2d84d44499e45f88d4
lucas-c11 <123lucaschi@gmail.com>
2026-01-26 10:16:13 -0500

    Move Orders Page and add route

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { HomePage } from "./pages/HomePage"
 import { Checkout } from "./pages/Checkout"
+import { Orders } from "./pages/Orders"
 import { Routes, Route } from "react-router"
 import './App.css'
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="checkout" element={<Checkout />} />
+        <Route path="orders" element={<Orders />}/>
       </Routes>
       
     </>
