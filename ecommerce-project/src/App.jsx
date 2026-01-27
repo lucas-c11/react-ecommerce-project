0606b5f01d04ea6d026c2cc131ea7d25163e5d2c
lucas-c11 <123lucaschi@gmail.com>
2026-01-27 10:50:29 -0500

    Add 404 NotFound page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import { HomePage } from "./pages/HomePage"
 import { Checkout } from "./pages/checkout/Checkout"
 import { Orders } from "./pages/Orders"
 import { Tracking } from "./pages/Tracking"
+import { NotFound } from "./pages/NotFound"
 
 import { Routes, Route } from "react-router"
 import './App.css'
@@ -14,6 +15,7 @@ function App() {
         <Route path="checkout" element={<Checkout />} />
         <Route path="orders" element={<Orders />}/>
         <Route path="tracking" element={<Tracking />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   )
---
+++ ecommerce-project/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+import "./NotFound.css"
+export function NotFound(){
+    return(
+        <div className="container">
+            <h1 className="not-found">404 Not Found</h1>
+        </div>
+    )
+}
