bf20b29066d7dd37fea561630374e0d6f99ec2e4
lucas-c11 <123lucaschi@gmail.com>
2026-01-28 21:16:17 -0500

    Get cart from backend and set the cart in the header

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { Header } from "../components/Header"
-import { products } from "../../starting-code/data/products"
 import HomeFavicon from "../assets/home-favicon.png"
 import Checkmark from "../assets/images/icons/checkmark.png"
 import "./HomePage.css"
 export function HomePage() {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
     useEffect(() => {
         axios.get("http://localhost:3000/api/products")
             .then((response) =>{
                 setProducts(response.data)
+            })
+
+        axios.get("http://localhost:3000/api/cart-items")
+            .then((response) =>{
+                setCart(response.data)
             })
     }, [])
     
@@ -19,7 +24,7 @@ export function HomePage() {
         <>
             <link rel="icon" href={HomeFavicon} />
             <title>Ecommerce Project</title>
-            <Header />
+            <Header cart={cart}/>
             <div className="home-page"> 
                 <div className="products-grid">
                     {products.map((product) => {
