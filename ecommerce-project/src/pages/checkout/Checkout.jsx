e464c2c9eb4b64d3f38ea0bcd76610f07e603713
lucas-c11 <123lucaschi@gmail.com>
2026-02-02 18:57:57 -0500

    Convert home and checkout to async await

--- ecommerce-project/src/pages/checkout/Checkout.jsx
+++ ecommerce-project/src/pages/checkout/Checkout.jsx
@@ -11,17 +11,18 @@ export function Checkout({ cart }) {
     const [deliveryOptions, setDeliveryOptions] = useState([]);
 
     useEffect(() => {
-        axios.get("/api/delivery-options?expand=estimatedDeliveryTime")
-            .then((response) => {
-                setDeliveryOptions(response.data);
-            })
-        axios.get("/api/payment-summary")
-            .then((response) => {
-                setPaymentSummary(response.data);
-            })
+        const fetchCheckoutData = async () => {
+            let response = await axios.get("/api/delivery-options?expand=estimatedDeliveryTime")
+            setDeliveryOptions(response.data);
+            response = await axios.get("/api/payment-summary")
+            setPaymentSummary(response.data);
+        };
+
+        fetchCheckoutData();
+
     }, [])
 
-    
+
     return (
         <>
             <link rel="icon" href={CartFavicon} />
@@ -31,7 +32,7 @@ export function Checkout({ cart }) {
                 <div className="page-title">Review your order</div>
                 <div className="checkout-grid">
                     <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
-                    <PaymentSummary paymentSummary={paymentSummary}/>
+                    <PaymentSummary paymentSummary={paymentSummary} />
                 </div>
             </div>
         </>
